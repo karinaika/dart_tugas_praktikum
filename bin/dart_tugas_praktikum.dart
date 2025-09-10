f315e60d92301c3a9777516e060620f1282f15ca
karinaika <karinaikaindasa@gmail.com>
2025-09-10 14:46:01 +0700

    first commit

---
+++ bin/dart_tugas_praktikum.dart
@@ -0,0 +1,5 @@
+import 'package:dart_tugas_praktikum/dart_tugas_praktikum.dart' as dart_tugas_praktikum;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_tugas_praktikum.calculate()}!');
+}
---
+++ test/dart_tugas_praktikum_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_tugas_praktikum/dart_tugas_praktikum.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
